5c7b5710bdc1ed66193016271bbb3e502b5259f8
Alexandre <alexandresilvestrin16@gmail.com>
2025-08-29 20:37:25 -0300

    anaylitcs vercel update

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import WhatsAppFloat from '../components/WhatsAppFloat'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <WhatsAppFloat />
+      <Analytics />
     </>
   )
 } 
